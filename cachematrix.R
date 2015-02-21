fd7dd4c09ed71a828900ea9fa10c9e37d27ac341
DataScienceRam <sriramag@yahoo.com>
2015-02-21 16:40:08 -0500

    Assignment for R programming
    Added more comments to make it clear for the readers.

--- cachematrix.R
+++ cachematrix.R
@@ -6,31 +6,47 @@
 ## This function will set and get the metrix and set and get the inverse of matrix.
 ## It takes one argument as matrix and returns list of functions to support cache operations.
 makeCacheMatrix <- function(x = matrix()) {
-  inverse <- NULL
-  set <- function(y) {
+  
+  inverse <- NULL               # First time we call this function inverse is NULL                        
+  
+  set <- function(y) {          # This internal function is used to set the metrix from outside.
     x <<- y
-    m <<- NULL
+    inverse <<- NULL
   }
-  get <- function() x
+  
+  get <- function() x           # This function will return the matrix.
+  
+  # Following functions will set and get inverse values
   setInverse <- function(i) inverse <<- i
   getInverse <- function() inverse
+  
+  # Following will return the list of functions.
   list(set = set, get = get,
        setInverse = setInverse,
        getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## The following function check to see if the cached value is present. If yes returns otherwise calculates caches.
 
 cacheSolve <- function(x, ...) {
-        
+  
+  # Get the inverse from the functions retuned and see it is already present.
   inverse <- x$getInverse()
   if(!is.null(inverse)) {
     message("getting inverse from cached data")
     return(inverse)
   }
+  
+  # Looks like inverse is not present. Get the metrix.
   data <- x$get()
+  
+  # Calculate the inverse.
   inverse <- solve(data, ...)
+  
+  # Cache it
   x$setInverse(inverse)
+  
+  # Return the inverse value back to user
   inverse
 }
